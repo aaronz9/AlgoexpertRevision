84601a27e254071adbffb47bf5a578ae3fd1c972
Aaron Fernandez <35059964+aaronz9@users.noreply.github.com>
2021-02-27 13:22:16 +1100

    Update Sumof2Number.py

--- Sumof2Number.py
+++ Sumof2Number.py
@@ -3,21 +3,21 @@ def twoNumberSum(array, targetSum):
     array.sort() #first we shall sort the array 
 	
 	
-	left = 0
-	right = len(array) - 1
+	left = 0 #starting index of the array
+	right = len(array) - 1 #last index of the array
 	
 	
-	while (left<right):
+	while (left<right): #while we don't reach the middle
 		
-		answer = array[left] + array[right]
+		answer = array[left] + array[right] # check the sum of first and last number
 		
-		if answer == targetSum:
-			return [array[left],array[right]]
+		if answer == targetSum: #if they are equal then all good.
+			return [array[left],array[right]] #return
 		
-		elif (answer < targetSum):
+		elif (answer < targetSum): # if the answer is falling short then obviosuly increment left as we sorted the array 
 			left+= 1
 			
-		elif (answer > targetSum):
+		elif (answer > targetSum): # if the anser is greater than move down from right.
 			right-= 1
 		
 		
